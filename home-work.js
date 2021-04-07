b19be372fc04f9d7ee31d47f0854da2ad7bc8f50
Valery213 <78694775+Valery213@users.noreply.github.com>
2021-04-07 22:23:21 +0300

    Add files via upload

--- home-work.js
+++ home-work.js
@@ -1,23 +1,31 @@
-const string = 'Германия, Берлин, 83млн, 357.4тыс.кв.км; Беларусь, Минск, 9.4млн, 207.6тыс.кв.км; Польша, Варшава, 37.9млн, 312.6тыс.кв.км; Франция, Париж, 67.8млн, 643.8тыс.кв.км; Нидерланды, Амстердам, 17.4млн, 41.5тыс.кв.км; Испания, Мадрид, 44.3млн, 505.9тыс.кв.км; Бельгия, Брюссель, 11.4млн, 30.5тыс.кв.км';
-const array = string.split('; ');
+'use strict'
+const string  		  = 'Германия, Берлин, 83млн, 357.4тыс.кв.км; Беларусь, Минск, 9.4млн, 207.6тыс.кв.км; Польша, Варшава, 37.9млн, 312.6тыс.кв.км; Франция, Париж, 67.8млн, 643.8тыс.кв.км; Нидерланды, Амстердам, 17.4млн, 41.5тыс.кв.км; Испания, Мадрид, 44.3млн, 505.9тыс.кв.км; Бельгия, Брюссель, 11.4млн, 30.5тыс.кв.км';
+const array1  		  = string.split('; ');
+const states  		  = [];
+const state   		  = {};
+let generalPopulation = 0;
+let totalArea 		  = 0;
 
-let square = 0;
-let population = 0;
+array1.map((value, key) => {
+	const array2 = value.split(', ');
 
-for (let i = 0; i < array.length; i++) {
-	
-	array2 = array[i].split(', ');
-	
-	population += +(array2[2].slice(0, -3));
-	square     += +(array2[3].slice(0, -9));
+	state[key] = {
+		country:    array2[0],
+		capital:    array2[1],
+		population: array2[2],
+		area:   	array2[3],
+	}
+	states[key] 	  = state[key];
+	generalPopulation += Number.parseFloat(array2[2]);
+	totalArea 		  += Number.parseFloat(array2[3]);
+})
+
+function NewCountry (name, generalPopulation, totalArea, capital) {
+	this.name 			   = name;
+	this.generalPopulation = generalPopulation + 'млн';
+	this.totalArea 		   = totalArea + 'тыс.кв.км';
+	this.capital 		   = capital;
 }
 
-function NewCity (name, population, square, capital) {
-	this.name 		= name;
-	this.population	= population;
-	this.square		= square;
-	this.capital	= capital;
-}
-
-const newCity = new NewCity('Лонг Лендс', population, square, 'Минск');
-console.log(newCity);
+const newCountry = new NewCountry('Лонг Лендс', generalPopulation, totalArea, 'Минск');
+console.log(newCountry);
